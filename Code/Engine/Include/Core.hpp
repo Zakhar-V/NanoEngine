3bb46017c30487bfeecd085d92634ee971bf16b4
Zakhar-V <Zakhar-V@users.noreply.github.com>
2018-06-04 15:45:32 +0700

    + core classes

---
+++ Code/Engine/Include/Core.hpp
@@ -0,0 +1,316 @@
+#pragma once
+
+#include "Base.hpp"
+
+//----------------------------------------------------------------------------//
+// RefCounted
+//----------------------------------------------------------------------------//
+
+//!
+class RefCounted : public NonCopyable
+{
+public:
+
+	//!
+	class WeakReference final : public NonCopyable
+	{
+	public:
+		friend class RefCounted;
+
+		//!
+		void AddRef(void);
+		//!
+		void Release(void);
+
+		RefCounted* GetPtr(void);
+
+	private:
+		//!
+		WeakReference(RefCounted* _ptr) : m_ptr(_ptr) { }
+		//!
+		~WeakReference(void)
+		{
+			ASSERT(m_ptr == nullptr && m_refCount == 0, "Incorrect deletion");
+		}
+
+		//!
+		void _Reset(void);
+
+		RefCounted* m_ptr;
+		int m_refCount = 1;
+	};
+
+	//!
+	RefCounted(void) = default;
+	//!
+	~RefCounted(void)
+	{
+		ASSERT(m_weakRef == nullptr && m_refCount == 0, "Incorrect deletion");
+	}
+
+	//!
+	void AddRef(void);
+	//! Safe increment a counter of references. Uses for weak references. 
+	bool SafeAddRef(void);
+	//!
+	void Release(void);
+
+	//!
+	WeakReference* GetWeakRef(void);
+
+protected:
+	//!
+	void _ResetRef(void);
+	//!
+	virtual void _DeleteThis(void)
+	{
+		_ResetRef();
+	}
+
+private:
+	//!
+	WeakReference* m_weakRef = nullptr;
+	int m_refCount = 0;
+};
+
+//----------------------------------------------------------------------------//
+// SharedPtr
+//----------------------------------------------------------------------------//
+
+//!
+enum NoAddRef_t { NoAddRef };
+
+//!
+template <class T> class SharedPtr
+{
+public:
+	//!
+	SharedPtr(void) = default;
+	//!
+	~SharedPtr(void)
+	{
+		if (m_ptr)
+			m_ptr->Release();
+	}
+	//!
+	SharedPtr(const SharedPtr& _ptr) : SharedPtr(_ptr.m_ptr) {}
+	//!
+	SharedPtr(SharedPtr&& _ptr) : m_ptr(_ptr.m_ptr)
+	{
+		_ptr.m_ptr = nullptr;
+	}
+	//!
+	SharedPtr(const T* _ptr) : m_ptr(const_cast<T*>(_ptr))
+	{
+		if (m_ptr)
+			m_ptr->AddRef();
+	}
+	//!
+	SharedPtr(const T* _ptr, NoAddRef_t) : m_ptr(const_cast<T*>(_ptr)) { }
+
+	//!
+	SharedPtr& operator = (const T* _ptr)
+	{
+		if (_ptr)
+			_ptr->AddRef();
+		if (m_ptr)
+			m_ptr->Release();
+		m_ptr = const_cast<T*>(_ptr);
+		return *this;
+	}
+	//!
+	SharedPtr& operator = (const SharedPtr& _ptr)
+	{
+		return *this = _p.m_ptr;
+	}
+	//!
+	SharedPtr& operator = (SharedPtr&& _ptr)
+	{
+		Swap(_ptr.m_ptr, m_ptr);
+		return *this;
+	}
+
+	//!
+	operator T* (void) const { return const_cast<T*>(m_ptr); }
+	//!
+	T* operator -> (void) const { return const_cast<T*>(m_ptr); }
+	//!
+	T& operator * (void) const { return *const_cast<T*>(p); }
+
+	//!
+	T* Get(void) const { return const_cast<T*>(m_ptr); }
+	//!
+	template <class X> X* Cast(void) const { return static_cast<X*>(const_cast<T*>(m_ptr)); }
+
+protected:
+	T* m_ptr = nullptr;
+};
+
+//----------------------------------------------------------------------------//
+// WeakRef
+//----------------------------------------------------------------------------//
+
+//!
+template <class T> class WeakRef
+{
+public:
+	//!
+	WeakRef(void) = default;
+	//!
+	~WeakRef(void) = default;
+	//!
+	WeakRef(const WeakRef& _ref) : m_ref(_ref.m_ref) { }
+	//!
+	WeakRef(WeakRef&& _ref) : m_ref(Move(_ref.m_ref)) { }
+	//!
+	WeakRef(const SharedPtr<T>& _ptr) : WeakRef(_ptr.Get()) { }
+	//!
+	WeakRef(const T* _ptr) : m_ref(_ptr ? const_cast<T*>(_ptr)->GetweakRef() : nullptr) { }
+
+	//!
+	WeakRef& operator = (const WeakRef& _ref)
+	{
+		m_ref = _ref.m_ref;
+		return *this;
+	}
+	//!
+	WeakRef& operator = (WeakRef&& _ref)
+	{
+		Swap(m_ref, _ref.m_ref);
+		return *this;
+	}
+	//!
+	WeakRef& operator = (const SharedPtr<T>& _ptr) { return *this = _ptr.Get(); }
+	//!
+	WeakRef& operator = (const T* _ptr)
+	{
+		m_ref = _ptr ? const_cast<T*>(_ptr)->GetweakRef() : nullptr;
+		return *this;
+	}
+
+	//!
+	operator SharedPtr<T>(void) const
+	{
+		return Lock();
+	}
+
+	//!
+	T* Get(void) const
+	{
+		return m_ref ? m_ref->GetPtr() : nullptr;
+	}
+
+	//!
+	SharedPtr<T> Lock(void) const
+	{
+		T* _ptr = Get();
+		if (_ptr && _ptr->SafeAddRef())
+			return SharedPtr<T>(_ptr, NoAddRef);
+
+		return nullptr;
+	}
+
+protected:
+	SharedPtr<RefCounted::WeakReference> m_ref;
+};
+
+//----------------------------------------------------------------------------//
+// Object
+//----------------------------------------------------------------------------//
+
+#ifndef _MSC_VER
+#define __SUPER(BASE) 	typedef BASE __super
+#else
+#define __SUPER(BASE)
+#	pragma warning(disable : 4307) // overflow of integer constant
+#endif
+
+//!
+#define RTTI(TYPE, BASE) \
+	__SUPER(BASE); \
+	enum : uint { TypeID = String::ConstHash(TYPE) }; \
+	uint GetTypeID(void) override { return TypeID; } \
+	bool IsTypeOf(uint _type) override { return _type == TypeID || __super::IsTypeOf(_type); } \
+	template <class T> bool IsTypeOf(void) { return IsTypeOf(T::TypeID); } \
+	static constexpr const char* TypeName = TYPE; \
+	const char* GetTypeName(void) override { return TypeName; }
+
+//!
+class Object : public RefCounted
+{
+public:
+	//!
+	enum : uint { TypeID = String::ConstHash("Object") };
+	//!
+	virtual uint GetTypeID(void) { return TypeID; }
+	//!
+	virtual bool IsTypeOf(uint _type) { return _type == TypeID; }
+	//!
+	template <class T> bool IsTypeOf(void) { return IsTypeOf(T::TypeID); }
+	//!
+	static constexpr const char* TypeName = "Object";
+	//!
+	virtual const char* GetTypeName(void) { return TypeName; }
+
+	//!
+	typedef SharedPtr<Object>(*Factory)(void);
+
+	//!
+	template <class T> static SharedPtr<Object> NewInstance() { return new T; }
+
+	//!
+	struct TypeInfo
+	{
+		//!
+		TypeInfo* SetFactory(Object::Factory _factory) { Factory = _factory; return this; }
+		//!
+		TypeInfo* SetFlags(uint _flags) { flags = _flags; return this; }
+		//!
+		TypeInfo* AddFlags(uint _flags) { flags |= _flags; return this; }
+		//!
+		bool HasAnyOfFlags(uint _flags) { return (flags & _flags) != 0; }
+
+
+		uint type;
+		const char* name;
+
+		Object::Factory Factory = nullptr;
+		uint flags = 0; //!< type-specific flags
+	};
+
+	//!
+	static TypeInfo* GetOrCreateTypeInfo(const char* _name);
+	//!
+	template <class T> static TypeInfo* GetOrCreateTypeInfo(void) { return GetOrCreateTypeInfo(T::TypeName); }
+
+	//!
+	static TypeInfo* GetTypeInfo(uint _type);
+	//!
+	template <class T> static TypeInfo* GetTypeInfo(void) { return GetTypeInfo(T::TypeID); }
+
+	//!
+	template <class T> static void Register(uint _flags = 0) { GetOrCreateTypeInfo<T>()->SetFactory(&Object::NewInstance<T>)->SetFlags(_flags); }
+
+	//!
+	static SharedPtr<Object> Create(uint _type, const char* _name);
+	//!
+	template <class T> static SharedPtr<T> Create(void) { return Create(T::TypeID, T::TypeName).Cast<T>(); }
+
+
+
+	//!
+	/*virtual void SendEvent(int _type, void* _data = nullptr, Object* _sender = nullptr) { }
+	//!
+	virtual void BroadcastEvent(int _type, void* _data = nullptr, Object* _sender = nullptr) { }
+	//!
+	virtual bool OnEvent(int _type, void* _data, Object* _sender) { return false; }*/
+
+
+protected: // private?
+
+	static HashMap<uint, TypeInfo> s_types;
+};
+
+//----------------------------------------------------------------------------//
+// 
+//----------------------------------------------------------------------------//
---
+++ Code/Engine/Source/Core.cpp
@@ -0,0 +1,167 @@
+#include "Core.hpp"
+
+//----------------------------------------------------------------------------//
+// RefCounted::WeakReference
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+void RefCounted::WeakReference::AddRef(void)
+{
+#ifndef NO_THREADS
+	AtomicAdd(m_refCount, 1);
+#else
+	++m_refCount;
+#endif
+}
+//----------------------------------------------------------------------------//
+void RefCounted::WeakReference::Release(void)
+{
+#ifndef NO_THREADS
+	if (AtomicSubtract(m_refCount, 1) == 1)
+		delete this;
+#else
+	if (!--m_refCount)
+		delete this;
+#endif
+}
+//----------------------------------------------------------------------------//
+RefCounted* RefCounted::WeakReference::GetPtr(void)
+{
+#ifndef NO_THREADS
+	return AtomicGet(m_ptr);
+#else
+	return m_ptr;
+#endif
+}
+//----------------------------------------------------------------------------//
+void RefCounted::WeakReference::_Reset(void)
+{
+#ifndef NO_THREADS
+	AtomicSet(m_ptr, nullptr);
+#else
+	m_ptr = nullptr;
+#endif
+	Release();
+}
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// RefCounted
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+void RefCounted::AddRef(void)
+{
+#ifndef NO_THREADS
+	AtomicAdd(m_refCount, 1);
+#else
+	++m_refCount;
+#endif
+}
+//----------------------------------------------------------------------------//
+bool RefCounted::SafeAddRef(void)
+{
+#ifndef NO_THREADS
+	for (;;)
+	{
+		int _rc = AtomicGet(m_refCount, MemoryOrder::Relaxed);
+
+		if (!_rc)
+			break;
+
+		if (AtomicCompareExchange(m_refCount, _rc, _rc + 1))
+			return true;
+	}
+	return false;
+#else
+	AddRef();
+	return true;
+#endif
+}
+//----------------------------------------------------------------------------//
+void RefCounted::Release(void)
+{
+#ifndef NO_THREADS
+	if (AtomicSubtract(m_refCount, 1) == 1)
+		_DeleteThis();
+#else
+	if (!--m_refCount)
+		_DeleteThis();
+#endif
+}
+//----------------------------------------------------------------------------//
+RefCounted::WeakReference* RefCounted::GetWeakRef(void)
+{
+#ifndef NO_THREADS
+	for (WeakReference* c = nullptr; (c = AtomicGet(m_weakRef, MemoryOrder::Relaxed)) == nullptr || c == (WeakReference*)-1;)
+	{
+		if (AtomicCompareExchange<WeakReference>(m_weakRef, (WeakReference*)0, (WeakReference*)-1))
+		{
+			AtomicSet(m_weakRef, new WeakReference(this));
+			break;
+		}
+	}
+#else
+	if (!m_weakRef)
+		m_weakRef = new WeakReference(this);
+#endif
+	return m_weakRef;
+}
+//----------------------------------------------------------------------------//
+void RefCounted::_ResetRef(void)
+{
+	if (m_weakRef)
+	{
+		m_weakRef->_Reset();
+		m_weakRef = nullptr;
+	}
+}
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// Object
+//----------------------------------------------------------------------------//
+
+HashMap<uint, Object::TypeInfo> Object::s_types;
+
+//----------------------------------------------------------------------------//
+Object::TypeInfo* Object::GetOrCreateTypeInfo(const char* _name)
+{
+	uint _type = String::Hash(_name);
+	auto _iter = s_types.Find(_type);
+	if (_iter != s_types.End())
+		return &_iter->second;
+
+	LOG("Register %s(0x%08x) typeinfo", _name, _type);
+
+	auto& _typeInfo = s_types[_type];
+	_typeInfo.type = _type;
+	_typeInfo.name = _name;
+
+	return &_typeInfo;
+}
+//----------------------------------------------------------------------------//
+Object::TypeInfo* Object::GetTypeInfo(uint _type)
+{
+	auto _iter = s_types.Find(_type);
+	if (_iter != s_types.End())
+		return &_iter->second;
+	return nullptr;
+}
+//----------------------------------------------------------------------------//
+SharedPtr<Object> Object::Create(uint _type, const char* _name)
+{
+	auto _iter = s_types.Find(_type);
+	if (_iter == s_types.End())
+	{
+		LOG("Error: Factory for '%s'(0x%08x) not found", _name, _type); // or better use of FATAL?
+		return nullptr;
+	}
+
+	return _iter->second.Factory();
+}
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+//
+//----------------------------------------------------------------------------//
